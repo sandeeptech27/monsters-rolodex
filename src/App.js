1f9704a2aac2e297589229c22899c8d3011f7d13
Sandeep N <sandeepnm7@gmail.com>
2023-02-12 14:05:07 +0530

    setState - Implementation

--- src/App.js
+++ src/App.js
@@ -4,22 +4,28 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      name: 'Atharva'
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Hi {this.state.name}
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <button onClick={() => {
+            this.setState({name: 'Sandeep'})
+          }}>
+            Change Name
+          </button>
         </header>
       </div>
     );
